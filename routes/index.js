686f6ea45959244adbbd961ad1a4964822e452aa
Thomas Schweich <thomas.schweich@gmail.com>
2018-10-08 18:46:53 -0400

    Begin backend implementation

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var schema = require('../schema')
+
+/**
+ * Get Save Number from url
+ * @param {String} url 
+ */
+function urlSn(url) {
+  return url.split('/')[1]
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html');
 });
 
-var player = {
-  name: String,
-  location: String,
-  status: String,
-  inventory: {
-      oxen: Number,
-      ammunition: Number
-  },
-  party: [],
-  offers: [
-      {
-          name: String,
-          request: {
-              item: String,
-              amount: Number
-          },
-          uid: String
-      },
-  ],
-  nearby: [
-      {   
-          uid: String,
-          inventory: {
-              oxen: Number,
-              ammunition: Number
-          }
-      },
-  ]
-}
+router.get('/new', function(req, res) {
+  schema.newPlayer().then(function(player) {
+    res.json(player)
+  }).catch(function(e) {
+    res.sendStatus(500)
+    console.log(e)
+  })
+})
 
+router.get(/^\/?.{6}\/?$/, function(req, res) {
+  schema.getPlayer(urlSn(req.url)).then(function(player) {
+    res.json(player)
+  }).catch(function(e) {
+    res.sendStatus(500)
+    console.log(e)
+  })
+})
+
+router.get(/^\/?.{6}\/next\/?$/), function(req, res) {
+  schema.getplayer(urlSn(req.url)).then(function(player) {
+    res.json(advance(player))
+  }).catch(function(e) {
+    res.sendStatus(500)
+    console.log(e)
+  })
+}
 
 module.exports = router;
---
+++ schema.js
@@ -0,0 +1,81 @@
+var mongoose = require('mongoose')
+mongoose.connect()
+
+
+var player = {
+    sn: String,
+    name: String,
+    turn: Number,
+    miles: Number,
+    location: String,
+    status: String,
+    inventory: {
+        oxen: Number,
+        ammunition: Number
+    },
+    party: [String],
+    offers: [
+        {
+            name: String,
+            request: {
+                item: String,
+                amount: Number
+            },
+            sn: String
+        },
+    ],
+    nearby: [
+        {   
+            uid: String,
+            inventory: {
+                oxen: Number,
+                ammunition: Number
+            }
+        },
+    ]
+  }
+
+var playerSchema = new mongoose.Schema(player)
+var Player = mongoose.model('Player', playerSchema)
+
+/**
+ * Adds an empty player to the database, returning its unique Save Number
+ */
+exports.newPlayer = async function newPlayer(properties={}) {
+    var sn, count
+    do {
+        sn = (Math.random() + 1).toString(36).substr(2, 6)
+        count = await Player.count({sn: sn}).exec()
+    } while(count)
+    var nplayer = new Player({sn: sn})
+    nplayer = {
+        ...nplayer,
+        ...properties
+    }
+    nplayer.save()
+    return nplayer
+}
+
+/**
+ * Get a player by Save Number
+ * @param {String} sn 
+ */
+exports.getplayer = async function getPlayer(sn) {
+    return await Player.findOne({sn: sn})
+}
+
+/**
+ * Modifies the player with save number sn
+ * Objects below the first level must be complete
+ * @param {String} sn The player's Save Number
+ */
+exports.modifyPlayer = async function modifyPlayer(sn, properties) {
+    var player = await getPlayer(sn)
+    player = {
+        ...player,
+        ...properties
+    }
+    player.save()
+}
+
+
