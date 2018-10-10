04bbfedf16fab25801c2692c09b8ef65516646a9
Thomas Schweich <thomas.schweich@gmail.com>
2018-10-10 18:57:34 -0400

    Begin actual next plugin

--- routes/next.js
+++ routes/next.js
@@ -1,16 +1,20 @@
 const game = require('../public/game.json')
 
+/**
+ * Generates balanced luck
+ */
+function luck() {}
+
 var states = {
     '': function (player, term, input) {
-        var locIdx
+        var recentIdx = 0
         game.locations.forEach(function(e, i) {
-            if (e.name === player.location.name) {
-                locIdx = i
+            if (e.distance < player && i > recentIdx) {
+                recentIdx = i
             }
         })
-        // TODO handle if cannot find
-        player.location = game.locations[locIdx + 1]
-        player.miles = game.locations[locIdx + 1].distance
+        player.location = game.locations[recentIdx + 1]
+        player.miles = game.locations[recentIdx + 1].distance
         term.terminate(player)
     }
 }
