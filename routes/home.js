f13474248555687b5faf776229a66a755e530ee3
djwensley <31390176+djwensley@users.noreply.github.com>
2018-10-11 04:27:35 -0400

    the world may never know

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,11 @@ var states = {
     '': function (player, term, input) {
         term.writeLine('You are in ' + player.location.name + ' at mile marker ' + player.miles)
         term.writeLine('Would you like to:')
-        player.location.features.forEach(function (e, i) {
-            feature = game.features[e]
+        //player.location.features.forEach(function (e, i) {
+            //feature = game.features[e]
 			term.writeLine("In home")
             //term.writeLine('(' + (i + 1) + ') ' + feature.description)
-        })
+        //})
         term.read(player, 'chosen')
     },
     'chosen': function(player, term, input) {
